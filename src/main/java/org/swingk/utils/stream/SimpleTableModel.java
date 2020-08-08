502f5389ec2006f29e36f11617900ed5b00368c2
Eli Rosenblum <parubok4@gmail.com>
2020-08-08 04:52:50 +0300

    SimpleTableModel extends DefaultTableModel.

--- src/main/java/org/swingk/utils/stream/SimpleTableModel.java
+++ src/main/java/org/swingk/utils/stream/SimpleTableModel.java
@@ -1,39 +1,86 @@
 package org.swingk.utils.stream;
 
-import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 import java.util.function.IntFunction;
 
 /**
- * Table model with fixed row/column count.
+ * Table model which extends {@link DefaultTableModel}.
  * Implements {@link IntFunction} to access row data objects.
- * Supports {@link #setValueAt(Object, int, int)} operation.
  *
  * @param <T> Type of the source stream elements.
  * @see SwingStreamUtils#toTableModel(Column[])
  */
-public final class SimpleTableModel<T> extends AbstractTableModel implements IntFunction<T> {
-    private final List<List<Object>> data;
-    private final int columnCount;
+public final class SimpleTableModel<T> extends DefaultTableModel implements IntFunction<T> {
+    private final List<T> rowsObjects;
     private final List<Class<?>> columnClasses;
-    private final List<String> columnNames;
     private final boolean[] columnsEditable;
 
     SimpleTableModel(List<List<Object>> data, List<Class<?>> columnClasses, List<String> columnNames,
                      boolean[] columnsEditable) {
-        super();
-        this.data = data;
-        this.columnCount = columnClasses.size();
+        super(data.size(), columnClasses.size());
+        final int colCount = getColumnCount();
+        this.rowsObjects = new ArrayList<>(getRowCount());
+        for (int row = 0; row < getRowCount(); row++) {
+            this.rowsObjects.add((T) data.get(row).get(colCount));
+            for (int column = 0; column < colCount; column++) {
+                Vector rowVector = (Vector) dataVector.elementAt(row);
+                rowVector.setElementAt(data.get(row).get(column), column);
+            }
+        }
+        setColumnIdentifiers(new Vector(columnNames));
         this.columnClasses = columnClasses;
-        this.columnNames = columnNames;
         this.columnsEditable = columnsEditable;
+    }
+
+    @Override
+    public void setNumRows(int rowCount) {
+        while (rowCount > rowsObjects.size()){
+            rowsObjects.add(null);
+        }
+        while (rowCount < rowsObjects.size()) {
+            rowsObjects.remove(rowsObjects.size() - 1);
+        }
+        assert rowCount == rowsObjects.size();
+        super.setNumRows(rowCount);
+    }
+
+    @Override
+    public void moveRow(int start, int end, int to) {
+        List<T> subList = rowsObjects.subList(start, end + 1);
+        List<T> temp = new ArrayList<>(subList);
+        subList.clear();
+        rowsObjects.addAll(to, temp);
+        super.moveRow(start, end, to);
+    }
+
+    @Override
+    public void removeRow(int row) {
+        rowsObjects.remove(row);
+        super.removeRow(row);
+    }
+
+    @Override
+    public void insertRow(int row, Vector rowData) {
+        rowsObjects.add(row, null);
+        super.insertRow(row, rowData);
     }
 
     /**
      * @return Data object (e.g. stream element) associated with this row.
      */
     public T getRowObject(int rowIndex) {
-        return (T) data.get(rowIndex).get(columnCount);
+        return rowsObjects.get(rowIndex);
+    }
+
+    /**
+     * @param rowIndex Row index.
+     * @param rowObject Data object (e.g. stream element) associated with this row.
+     */
+    public void setRowObject(int rowIndex, T rowObject) {
+        rowsObjects.set(rowIndex, rowObject);
     }
 
     @Override
@@ -42,38 +89,18 @@ public final class SimpleTableModel<T> extends AbstractTableModel implements Int
     }
 
     @Override
-    public int getRowCount() {
-        return data.size();
-    }
-
-    @Override
-    public int getColumnCount() {
-        return columnCount;
-    }
-
-    @Override
-    public String getColumnName(int columnIndex) {
-        return columnNames.get(columnIndex);
-    }
-
-    @Override
     public Class<?> getColumnClass(int columnIndex) {
+        if (columnIndex >= columnClasses.size()) {
+            return Object.class;
+        }
         return columnClasses.get(columnIndex);
     }
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
+        if (columnIndex >= columnsEditable.length) {
+            return false;
+        }
         return columnsEditable[columnIndex];
-    }
-
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        return data.get(rowIndex).get(columnIndex);
-    }
-
-    @Override
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        data.get(rowIndex).set(columnIndex, aValue);
-        fireTableCellUpdated(rowIndex, columnIndex);
     }
 }
--- src/test/java/org/swingk/utils/stream/SimpleTableModelTest.java
+++ src/test/java/org/swingk/utils/stream/SimpleTableModelTest.java
@@ -13,6 +13,8 @@ class SimpleTableModelTest {
         List<List<Object>> data = asList(asList(1, "A", "data1"), asList(2, "B", "data2"));
         SimpleTableModel<String> model = new SimpleTableModel<>(data, asList(Integer.class, String.class),
                 asList("col1", "col2"), new boolean[]{false, true});
+        Assertions.assertEquals(2, model.getRowCount());
+        Assertions.assertEquals(2, model.getColumnCount());
         Assertions.assertEquals(1, model.getValueAt(0, 0));
         Assertions.assertEquals("A", model.getValueAt(0, 1));
         Assertions.assertEquals(2, model.getValueAt(1, 0));
@@ -27,5 +29,20 @@ class SimpleTableModelTest {
         Assertions.assertTrue(model.isCellEditable(1, 1));
         Assertions.assertEquals("data1", model.getRowObject(0));
         Assertions.assertEquals("data2", model.getRowObject(1));
+    }
+
+    @Test
+    void add_remove_row_1() {
+        List<List<Object>> data = asList(asList(1, "A", "data1"), asList(2, "B", "data2"));
+        SimpleTableModel<String> model = new SimpleTableModel<>(data, asList(Integer.class, String.class),
+                asList("col1", "col2"), new boolean[]{false, true});
+        model.removeRow(0);
+        Assertions.assertEquals(1, model.getRowCount());
+        Assertions.assertEquals("data2", model.getRowObject(0));
+        model.addRow(new Object[]{3, "C"});
+        Assertions.assertEquals(2, model.getRowCount());
+        Assertions.assertNull(model.getRowObject(1));
+        model.setRowObject(1, "data3");
+        Assertions.assertEquals("data3", model.getRowObject(1));
     }
 }
